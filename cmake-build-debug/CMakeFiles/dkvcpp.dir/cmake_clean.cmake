9828c549179fbd44da317b046f1cf294c9becdb1
nireo <eemil.lehtikangas1@gmail.com>
2021-03-30 00:23:33 +0300

    initial commit

---
+++ cmake-build-debug/CMakeFiles/dkvcpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dkvcpp.dir/main.cpp.o"
+  "dkvcpp"
+  "dkvcpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dkvcpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
